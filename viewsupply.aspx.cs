677b961a9d84e1200de35875650a554994117f8e
decrescentisr <decrescentisr@gmail.com>
2019-02-20 12:36:14 -0600

    Update viewsupply.aspx.cs

--- viewsupply.aspx.cs
+++ viewsupply.aspx.cs
@@ -30,37 +30,38 @@ namespace SchoolDistWeb
                 Server.Transfer("login.aspx", true);
             }
         }
-
+        //Method that allows users to view school supply information
         protected void btnSearch_Click(object sender, EventArgs e)
         {
-            string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString; //Connects to database through ConnectionString in the Web.Config file
             using (SqlConnection con = new SqlConnection(constr))
             {
-                using (SqlCommand cmd = new SqlCommand())
+                using (SqlCommand cmd = new SqlCommand()) //Represents a Transact-SQL statement or stored procedure to execute against a SQL Server database. This class cannot be inherited. 
                 {
-                    string sql = "SELECT Teacher, Supply1, Supply2, Supply3, Supply4, Supply5, Supply6, Supply7, Supply8 FROM Supplies";
-                    if (!string.IsNullOrEmpty(txtLastName.Text.Trim()))
+                    string sql = "SELECT Teacher, Supply1, Supply2, Supply3, Supply4, Supply5, Supply6, Supply7, Supply8 FROM Supplies"; //SQL command which allows users to view all data from Teacher table
+                    if (!string.IsNullOrEmpty(txtLastName.Text.Trim())) //Lists all teacher school supply information if textbox control is empty
                     {
-                        sql += " WHERE Teacher LIKE @Teacher + '%'";
+                        sql += " WHERE Teacher LIKE @Teacher + '%'"; //Allows users to view information by teacher name
                         cmd.Parameters.AddWithValue("@Teacher", txtLastName.Text.Trim());
                     }
-                    cmd.CommandText = sql;
-                    cmd.Connection = con;
-                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    cmd.CommandText = sql; //Gets or sets the Transact-SQL statement, table name or stored procedure to execute at the data source.
+                    cmd.Connection = con; //Connects to command
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd)) //Represents a set of data commands and a database connection that are used to fill the DataSet and update a SQL Server database.  
                     {
-                        DataTable dt = new DataTable();
-                        sda.Fill(dt);
-                        grdSupply.DataSource = dt;
-                        grdSupply.DataBind();
+                        DataTable dt = new DataTable(); //Creates new data table
+                        sda.Fill(dt); //Fills data table
+                        grdSupply.DataSource = dt; //Uses data source to fill gridview control
+                        grdSupply.DataBind(); //Binds data to grid view
                     }
                 }
             }
 
         }
-
+        
+        //Method that allows users to print gridview information if pressing Print button
         protected void btnPrints_Click(object sender, EventArgs e)
         {
-            btnPrints.Attributes.Add("onclick", "return printing()");
+            btnPrints.Attributes.Add("onclick", "return printing()"); //Creates JavaScript onclick method that populates print options screen
         }
     }
 }
